acf05c02f40b100bd162003edf80d12ccc1e1cc4
Noriend <dian.yordanov.z@gmail.com>
2018-09-09 14:43:46 +0300

    small tests

--- windows_bat_file_for-anaconda.bat
+++ windows_bat_file_for-anaconda.bat
@@ -1,5 +1,6 @@
 @echo off
+SET FileName=output13.txt
 
-cmd /k "cd /d D:\AnacondaPython\Scripts & activate & cd /d & cd D:\startup-name-generator\startup-name-generator & python generate.py -e 500 -n 30 -t 0.7 -s models/gallicc_500epochs.h5 wordlists/country.txt > output11.txt & cat output11.txt"
+cmd /k "cd /d D:\AnacondaPython\Scripts & activate & cd /d & cd D:\startup-name-generator\startup-name-generator & python generate.py -e 100 -n 30 -t 0.7 -s models/gallicc_500epochs.h5 wordlists/country.txt > %FileName% & cat %FileName%"
 
 pause
