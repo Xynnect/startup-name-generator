30081e34cad1ee7318eb518bfecc3e7c7d1ae59a
Noriend <dian.yordanov.z@gmail.com>
2018-09-09 04:52:57 +0300

    some tests

--- windows_bat_file_for-anaconda.bat
+++ windows_bat_file_for-anaconda.bat
@@ -1,6 +1,6 @@
 @echo off
 
 D:
-cmd /k "cd /d D:\AnacondaPython\Scripts & activate & cd /d & cd D:\startup-name-generator\startup-name-generator & python generate.py -e 500 -n 10 -t 0.7 -s models/gallicc_500epochs.h5 wordlists/gallic.txt > output4.txt & cat output4.txt"
+cmd /k "cd /d D:\AnacondaPython\Scripts & activate & cd /d & cd D:\startup-name-generator\startup-name-generator & python generate.py -e 500 -n 30 -t 0.7 -s models/gallicc_500epochs.h5 wordlists/gallic.txt > output6.txt & cat output4.txt"
 
 pause
