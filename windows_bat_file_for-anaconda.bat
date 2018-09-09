bad5e803ce4eafe583f175b7a3b30154fdacd03a
Noriend <dian.yordanov.z@gmail.com>
2018-09-09 17:45:36 +0300

    even more tests
    http://alpha-epsilon.de/python/2018/04/04/an-lstm-based-startup-name-generator/
    https://github.com/AlexEngelhardt/startup-name-generator/tree/master/sample-outputs/
    https://github.com/AlexEngelhardt/startup-name-generator
    https://www.kdnuggets.com/2018/04/neural-network-startup-name-generator.html

--- windows_bat_file_for-anaconda.bat
+++ windows_bat_file_for-anaconda.bat
@@ -1,6 +1,6 @@
 @echo off
-SET FileName=output13.txt
+SET FileName=output19.txt
 
-cmd /k "cd /d D:\AnacondaPython\Scripts & activate & cd /d & cd D:\startup-name-generator\startup-name-generator & python generate.py -e 100 -n 30 -t 0.7 -s models/gallicc_500epochs.h5 wordlists/country.txt > %FileName% & cat %FileName%"
+cmd /k "cd /d D:\AnacondaPython\Scripts & activate & cd /d & cd D:\startup-name-generator\startup-name-generator & python generate.py -e 100 -n 30 -t 0.5 wordlists/cleaned-countries-data.txt > %FileName% & cat %FileName%"
 
 pause
